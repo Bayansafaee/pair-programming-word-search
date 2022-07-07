0fc0b4764f5cccee721714725777bb810a4b5c9b
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-06 17:57:41 -0700

    implementing a word search solver, as a function that receives a 2D array of letters and a word

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = (matrix) => {
+  let result = [];
+  let newArray = [];
+  for (let j = 0; j < matrix[0].length; j++) {
+    newArray = [];
+    for (let i = 0; i < matrix.length; i++) {
+      newArray.push(matrix[i][j]);
     }
-}
+    result.push(newArray);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+    
+  let verticalLetters = transpose(letters);
+
+  const verticalJoin = verticalLetters.map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
+
+ 
